92914ebe4c74d97014dfb1bceb9b56c774a1f67d
ganadaramaba <cartoon_____@naver.com>
2022-06-09 20:02:53 +0900

    second commit

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const port = 3000;
 
 app.use(express.json());
 
+const string = require("./router/string");
+app.use("/string", string);
+
+const list = require("./router/list");
+app.use("/list", list);
+
+const set = require("./router/set");
+app.use("/set", set);
+
+const sortedSet = require("./router/sortedSet");
+app.use("/sortedSet", sortedSet);
+
+const hash = require("./router/hash");
+app.use("/hash", hash);
+
 app.listen(port, async (req, res) => {
     console.log("서버 실행");
 });
